5443b9d9f4a37fdce2b913dff7176c6b196599b3
Lewis Reid <lreidwilliams@gmail.com>
2022-01-26 23:56:03 +1000

    added failing test for combination case

--- test/sliding_tiles/turn_test.exs
+++ test/sliding_tiles/turn_test.exs
@@ -39,6 +39,14 @@ defmodule SlidingTiles.TurnTest do
       assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {2, 0})
       assert %Tabletop.Piece{id: 8} = Tabletop.get_piece(board, {3, 0})
     end
+
+    test "[2 _ _ 2] -> [_ _ _ 4]" do
+      board = SlidingTiles.fresh_board()
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(2), {3, 0}})
+        |> SlidingTiles.Turn.slide(:right)
+      assert not Tabletop.occupied?(board, {0, 0})
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {3, 0})
+    end
   end
 
 end
