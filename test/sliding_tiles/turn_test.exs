d2057feb2c044e2491dd90e03950f8f876d09ee8
Lewis Reid <lreidwilliams@gmail.com>
2022-01-26 23:54:34 +1000

    improved slide right tests by comparing expected tile values as well as position

--- test/sliding_tiles/turn_test.exs
+++ test/sliding_tiles/turn_test.exs
@@ -3,41 +3,41 @@ defmodule SlidingTiles.TurnTest do
   doctest SlidingTiles.Turn
 
   describe "sliding right" do
-    test "[O _ _ _] -> [_ _ _ O]" do
+    test "[2 _ _ _] -> [_ _ _ 2]" do
       board = SlidingTiles.fresh_board()
         |> SlidingTiles.Turn.slide(:right)
       assert not Tabletop.occupied?(board, {0, 0})
-      assert Tabletop.occupied?(board, {3, 0})
+      assert %Tabletop.Piece{id: 2} = Tabletop.get_piece(board, {3, 0})
     end
 
-    test "[O O _ _] -> [_ _ O O]" do
+    test "[2 4 _ _] -> [_ _ 2 4]" do
       board = SlidingTiles.fresh_board()
-        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(3), {1, 0}})
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(4), {1, 0}})
         |> SlidingTiles.Turn.slide(:right)
       assert not Tabletop.occupied?(board, {0, 0})
       assert not Tabletop.occupied?(board, {1, 0})
-      assert Tabletop.occupied?(board, {2, 0})
-      assert Tabletop.occupied?(board, {3, 0})
+      assert %Tabletop.Piece{id: 2} = Tabletop.get_piece(board, {2, 0})
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {3, 0})
     end
 
-    test "[O _ _ O] -> [_ _ O O]" do
+    test "[2 _ _ 4] -> [_ _ 2 4]" do
       board = SlidingTiles.fresh_board()
-        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(3), {3, 0}})
-        |> SlidingTiles.Turn.slide(:right)
-      assert not Tabletop.occupied?(board, {0, 0})
-      assert Tabletop.occupied?(board, {2, 0})
-      assert Tabletop.occupied?(board, {3, 0})
-    end
-
-    test "[O _ O O] -> [_ O O O]" do
-      board = SlidingTiles.fresh_board()
-        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(3), {2, 0}})
         |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(4), {3, 0}})
         |> SlidingTiles.Turn.slide(:right)
       assert not Tabletop.occupied?(board, {0, 0})
-      assert Tabletop.occupied?(board, {1, 0})
-      assert Tabletop.occupied?(board, {2, 0})
-      assert Tabletop.occupied?(board, {3, 0})
+      assert %Tabletop.Piece{id: 2} = Tabletop.get_piece(board, {2, 0})
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {3, 0})
+    end
+
+    test "[2 _ 4 8] -> [_ 2 4 8]" do
+      board = SlidingTiles.fresh_board()
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(4), {2, 0}})
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(8), {3, 0}})
+        |> SlidingTiles.Turn.slide(:right)
+      assert not Tabletop.occupied?(board, {0, 0})
+      assert %Tabletop.Piece{id: 2} = Tabletop.get_piece(board, {1, 0})
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {2, 0})
+      assert %Tabletop.Piece{id: 8} = Tabletop.get_piece(board, {3, 0})
     end
   end
 
