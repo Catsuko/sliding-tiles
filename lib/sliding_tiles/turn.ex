8cf57d2fc81429f27c4a2274b7c043179fc2ad1c
Lewis Reid <lreidwilliams@gmail.com>
2022-02-01 23:06:04 +1000

    test coverage for sliding left

--- lib/sliding_tiles/turn.ex
+++ lib/sliding_tiles/turn.ex
@@ -1,24 +1,26 @@
 defmodule SlidingTiles.Turn do
 
   def slide(board, :up) do
-    slide(board, {0, 1})
+    take_turn(board, [], {0, 1})
   end
 
   def slide(board, :left) do
-    slide(board, {-1, 0})
+    positions = Enum.map(0..3, fn y -> {0, y} end)
+    take_turn(board, positions, {-1, 0})
   end
 
   def slide(board, :right) do
-    slide(board, {-1, 0})
+    positions = Enum.map(0..3, fn y -> {3, y} end)
+    take_turn(board, positions, {1, 0})
   end
 
   def slide(board, :down) do
-    slide(board, {0, -1})
+    take_turn(board, [], {0, -1})
   end
 
-  def slide(board, slide_direction) do
-    moves = 0..3
-      |> Enum.map(fn y -> moves_from(board, {3, y}, slide_direction) end)
+  defp take_turn(board, starting_positions, direction) do
+    moves = starting_positions
+      |> Enum.map(fn pos -> moves_from(board, pos, direction) end)
       |> List.flatten()
     Tabletop.take_turn(board, moves)
   end
@@ -29,29 +31,30 @@ defmodule SlidingTiles.Turn do
         moves
       Tabletop.occupied?(board, pos) ->
         {{next_x, next_y} = next_pos, combined_moves} = check_for_combinations(board, moves, pos, dir)
-        updated_moves = add_moves_for_position(pos, combined_moves, empty_count)
+        updated_moves = add_moves_for_position(pos, dir, combined_moves, empty_count)
         next_distance = abs(x - next_x) + abs(y - next_y) - 1
         moves_from(board, next_pos, dir, updated_moves, empty_count + next_distance)
       true ->
-        moves_from(board, {x + x_dir, y + y_dir}, dir, moves, empty_count + 1)
+        moves_from(board, {x - x_dir, y - y_dir}, dir, moves, empty_count + 1)
     end
   end
 
-  defp check_for_combinations(board, moves, pos, {x_dir, y_dir} = dir) do
+  defp check_for_combinations(board, moves, pos, {x_dir, y_dir}) do
     %Tabletop.Piece{id: id} = Tabletop.get_piece(board, pos)
-    case SlidingTiles.find_next_piece(board, pos, dir) do
+    case SlidingTiles.find_next_piece(board, pos, {-x_dir, -y_dir}) do
       {{next_x, next_y} = next_pos, %Tabletop.Piece{id: ^id} = piece} ->
-        {{next_x + x_dir, next_y + y_dir}, moves ++ [remove: next_pos, add: {SlidingTiles.double(piece), pos}]}
+        {{next_x - x_dir, next_y - y_dir}, moves ++ [remove: next_pos, add: {SlidingTiles.double(piece), pos}]}
       {next_pos, _} ->
         {next_pos, moves}
     end
   end
 
-  defp add_moves_for_position({x, y} = pos, moves, empty_count) do
+  defp add_moves_for_position({x, y} = pos, {x_dir, y_dir}, moves, empty_count) do
     if empty_count == 0 do
       moves
     else
-      moves ++ [move: {pos, {x + empty_count, y}}]
+      destination = {x + (x_dir * empty_count), (y + y_dir * empty_count)}
+      moves ++ [move: {pos, destination}]
     end
   end
 
--- test/sliding_tiles/turn_test.exs
+++ test/sliding_tiles/turn_test.exs
@@ -2,6 +2,53 @@ defmodule SlidingTiles.TurnTest do
   use ExUnit.Case
   doctest SlidingTiles.Turn
 
+  describe "sliding left" do
+    test "[2 _ _ _] -> [2 _ _ _]" do
+      board = SlidingTiles.fresh_board()
+        |> SlidingTiles.Turn.slide(:left)
+      assert %Tabletop.Piece{id: 2} = Tabletop.get_piece(board, {0, 0})
+    end
+
+    test "[_ _ _ 2] -> [2 _ _ _]" do
+      board = SlidingTiles.fresh_board()
+        |> Tabletop.Actions.apply(:move, {{0, 0}, {3, 0}})
+        |> SlidingTiles.Turn.slide(:left)
+      assert %Tabletop.Piece{id: 2} = Tabletop.get_piece(board, {0, 0})
+      assert not Tabletop.occupied?(board, {3, 0})
+    end
+
+    test "[_ 2 _ 4] -> [2 4 _ _]" do
+      board = SlidingTiles.fresh_board()
+        |> Tabletop.Actions.apply(:move, {{0, 0}, {1, 0}})
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(4), {3, 0}})
+        |> SlidingTiles.Turn.slide(:left)
+      assert %Tabletop.Piece{id: 2} = Tabletop.get_piece(board, {0, 0})
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {1, 0})
+      assert not Tabletop.occupied?(board, {3, 0})
+    end
+
+    test "[2 2 _ _] -> [4 _ _ _]" do
+      board = SlidingTiles.fresh_board()
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(2), {1, 0}})
+        |> SlidingTiles.Turn.slide(:left)
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {0, 0})
+      assert not Tabletop.occupied?(board, {1, 0})
+    end
+
+    test "[2 2 2 2] -> [4 4 _ _]" do
+      board = SlidingTiles.fresh_board()
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(2), {1, 0}})
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(2), {2, 0}})
+        |> Tabletop.Actions.apply(:add, {SlidingTiles.tile(2), {3, 0}})
+        |> SlidingTiles.Turn.slide(:left)
+
+      assert not Tabletop.occupied?(board, {2, 0})
+      assert not Tabletop.occupied?(board, {3, 0})
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {0, 0})
+      assert %Tabletop.Piece{id: 4} = Tabletop.get_piece(board, {1, 0})
+    end
+  end
+
   describe "sliding right" do
     test "[2 _ _ _] -> [_ _ _ 2]" do
       board = SlidingTiles.fresh_board()
