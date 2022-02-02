060ce817ceef249d1e1a3a0e3a59305f65c9c02d
Lewis Reid <lreidwilliams@gmail.com>
2022-02-03 00:03:08 +1000

    added effect for adding a tile at the end of each turn and stub for checking game over

--- lib/sliding_tiles.ex
+++ lib/sliding_tiles.ex
@@ -5,6 +5,14 @@ defmodule SlidingTiles do
       |> Tabletop.Actions.apply(:add, {tile(2), {0,0}})
   end
 
+  def new do
+    Tabletop.Board.square(4)
+      |> Tabletop.Board.assign(game_over: false)
+      |> Tabletop.Board.add_effects(&add_random_tile/1)
+      |> Tabletop.Board.add_effects(&check_game_over/1)
+      |> Tabletop.take_turn([])
+  end
+
   def tile(n) do
     %Tabletop.Piece{id: n}
   end
@@ -18,6 +26,26 @@ defmodule SlidingTiles do
       |> Stream.drop_while(fn {pos, piece} -> pos == starting_pos or piece == nil end)
       |> Stream.take(1)
       |> Enum.at(0, {{-1, -1}, nil})
+  end
+
+  def add_random_tile(board) do
+    positions = empty_positions(board)
+    if Enum.any?(positions) do
+      random_position = Enum.random(positions)
+      Tabletop.Actions.apply(board, :add, {tile(2), random_position})
+    else
+      board
+    end
+  end
+
+  def check_game_over(board) do
+    # TODO: any possible moves? -> mark game over!
+    board
+  end
+
+  def empty_positions(%Tabletop.Board{pieces: pieces} = board) do
+    Map.keys(pieces)
+      |> Enum.reject(fn pos -> Tabletop.occupied?(board, pos) end)
   end
 
 end
---
+++ test/sliding_tiles/effects_test.exs
@@ -0,0 +1,27 @@
+defmodule SlidingTiles.EffectsTest do
+  use ExUnit.Case
+
+  test "board starts with a 2 tile at a random position" do
+    %Tabletop.Board{pieces: pieces} = SlidingTiles.new()
+    piece = Map.values(pieces)
+      |> Enum.find(fn item -> item end)
+    assert %Tabletop.Piece{id: 2} = piece
+  end
+
+  describe "end of turn" do
+    test "adds another 2 tile at a random position" do
+      %Tabletop.Board{pieces: pieces} = SlidingTiles.new()
+        |> SlidingTiles.Turn.slide(:down)
+      tile_count = Map.values(pieces)
+        |> Enum.filter(fn item -> item end)
+        |> Enum.count(fn %Tabletop.Piece{id: id} -> id == 2 end)
+      assert tile_count == 2
+    end
+
+    test "game is not over when board is full but moves can be made" do
+    end
+
+    test "game is over when board is full with no moves" do
+    end
+  end
+end
